d6581cf307ad350160bff9bae63408efdfad24df
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-02-10 16:32:57 +0545

    Project commit day 2

---
+++ Broadway.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic=Broadway.ConsoleApp.Logic;
+using MathSub = Broadway.ConsoleApp.Subject.Math;
+using Sub= Broadway.ConsoleApp.Subject;
+using Broadway.Project.Models;
+using Broadway.VB;
+
+namespace Broadway.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOps();
+
+            Console.ReadLine();
+        }
+
+         public static void MathOps()
+        {
+            Console.Write("Enter the First Number: ");
+            var a = Console.ReadLine();
+            Console.Write("Enter the Second Number: ");
+            var b = Console.ReadLine();
+
+            var num_a = Convert.ToDecimal(a);
+            var num_b = Convert.ToDecimal(b);
+
+            Logic.Math math=new Logic.Math();
+
+            Console.WriteLine("Addition "+math.Add(num_a, num_b));
+
+            Console.WriteLine($"Subtract {math.Subtract(num_a, num_b)}");
+
+            Console.WriteLine("Multiplication {0}", math.Multiply(num_a, num_b));
+
+            Console.WriteLine($"Division {math.Divide(num_a, num_b)}");
+
+        }
+
+
+    }
+
+
+   
+
+    
+   
+}
---
+++ Broadway.VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Broadway.VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Broadway.VB")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("64a4ecce-04f0-4da9-9628-e8f60f5da638")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
