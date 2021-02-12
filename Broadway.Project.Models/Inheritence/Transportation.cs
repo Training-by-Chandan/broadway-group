51bf65f298e67e421e89cd8744b3b006d7144f79
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-02-12 16:13:04 +0545

    inhertitence , polymorhpism : day 4

--- Broadway.Project.Models/Inheritence/Transportation.cs
+++ Broadway.Project.Models/Inheritence/Transportation.cs
@@ -8,15 +8,47 @@ namespace Broadway.Project.Models.Inheritence
 {
     public class Transportation
     {
+        public Transportation()
+        {
+
+        }
+        public Transportation(string str)
+        {
+            this.str = str;
+        }
+
+        public Transportation(string str, int i)
+        {
+
+        }
+        public Transportation(int i, string str)
+        {
+
+        }
         protected string str = "test string";
         protected string MoveStr(string mode)
         {
             return "Yay! I can move in "+mode;
         }
+
+        public virtual void FunctionOne()
+        {
+            Console.WriteLine("I am from base class Transportation");
+        }
     }
 
     public class LandTransport : Transportation
     {
+        public LandTransport() : base()
+        {
+
+        }
+
+        public LandTransport(string str) : base(str)
+        {
+
+        }
+
         public void Move()
         {
             Console.WriteLine(this.MoveStr("land"));
@@ -26,6 +58,11 @@ namespace Broadway.Project.Models.Inheritence
         {
             this.str = str;
             
+        }
+
+        public override void FunctionOne()
+        {
+            Console.WriteLine("I am from child class LandTransport");
         }
     }
 
@@ -44,11 +81,13 @@ namespace Broadway.Project.Models.Inheritence
             Console.WriteLine(this.MoveStr("water"));
         }
     }
-    public class SpaceTransport : Transportation
+    public class SpaceTransport : AirTransport
     {
         public void Move()
         {
             Console.WriteLine(this.MoveStr("space"));
         }
     }
+
+
 }
