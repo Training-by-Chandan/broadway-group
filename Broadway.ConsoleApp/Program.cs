b4eeb72cc5c35fdf311e6d066c1984a015859c33
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-03-16 15:54:24 +0545

    Encryption and Decryption Functionality added

--- Broadway.ConsoleApp/Program.cs
+++ Broadway.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Broadway.ConsoleApp.AbstractClass;
@@ -52,7 +53,10 @@ namespace Broadway.ConsoleApp
                 #endregion
                 //ReferenceExample();
 
-                DelegateEventsExample();
+                //DelegateEventsExample();
+
+                PasswordEncryptionAndDecryption();
+
 
                 Console.WriteLine("Continue (Y/N)");
                 res = Console.ReadLine();
@@ -421,6 +425,80 @@ namespace Broadway.ConsoleApp
         {
             Console.WriteLine("Calling from program.cs");
             return 0;
+        }
+
+        private const string SecurityKey = "bfc4ae7f-1116-4b57-b6e3-5edd5dc35ea6";
+
+        //This method is used to convert the plain text to Encrypted/Un-Readable Text format.
+        public static string EncryptPlainTextToCipherText(string PlainText)
+        {
+            // Getting the bytes of Input String.
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(SecurityKey));
+            //De-allocatinng the memory after doing the Job.
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            //Assigning the Security key to the TripleDES Service Provider.
+            objTripleDESCryptoService.Key = securityKeyArray;
+            //Mode of the Crypto service is Electronic Code Book.
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            //Padding Mode is PKCS7 if there is any extra byte is added.
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+            //Transform the bytes array to resultArray
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+            objTripleDESCryptoService.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        //This method is used to convert the Encrypted/Un-Readable Text back to readable  format.
+        private static string DecryptCipherTextToPlainText(string CipherText, string securityKey)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(CipherText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+
+            //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(securityKey));
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            //Assigning the Security key to the TripleDES Service Provider.
+            objTripleDESCryptoService.Key = securityKeyArray;
+            //Mode of the Crypto service is Electronic Code Book.
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            //Padding Mode is PKCS7 if there is any extra byte is added.
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
+            //Transform the bytes array to resultArray
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            objTripleDESCryptoService.Clear();
+
+            //Convert and return the decrypted data/byte into string format.
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+
+
+        static void PasswordEncryptionAndDecryption()
+        {
+            string str = "This is test string.";
+            string encryptedText=EncryptPlainTextToCipherText(str);
+            Console.WriteLine("Encrypted Text"+encryptedText);
+
+            //var falsedecryptedText = DecryptCipherTextToPlainText(encryptedText, "bfc4ae7f-1116-4b57-b6e3-5edd5dc35ea7");
+            //Console.WriteLine("False Decryption => "+falsedecryptedText);
+
+            var decryptedText = DecryptCipherTextToPlainText(encryptedText, "bfc4ae7f-1116-4b57-b6e3-5edd5dc35ea6");
+            Console.WriteLine("Decryption => " + decryptedText);
+
+
+
         }
     }
 
